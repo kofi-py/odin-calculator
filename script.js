a559a4a1236d3a159cf09d46af0b5a8cdf31fb4a
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2025-06-03 17:30:09 -0400

    Create operate function.

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ let firstNumber = "";
 let secondNumber = "";
 let currentOperator = null;
 let shouldResetScreen = false;
+
+// Operate Function
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
